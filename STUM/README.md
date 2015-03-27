9e039d3852d6c0412abbc56bc7e66ee65507fbc0
MinJeong <ymj931@gmail.com>
2015-03-27 19:44:23 +0900

    parse android tutorial

---
+++ STUM/README.md
@@ -0,0 +1,5 @@
+###parse tutorial###
+
+2015 - 03 - 27
+
+parse android 기본 튜토리얼 하던 것
---
+++ STUM/app/src/main/java/com/example/stum/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.stum;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "WKmpjpL6bd3ANG5MlE4rzw1aBlknyXAI9c9XsGgW", "iM0bGYmm06nJTDj6cRqNV6B7sD6w85u5MN98VG1l");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
